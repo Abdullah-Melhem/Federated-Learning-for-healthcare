c5cf41a3b575c70d5ab76737c29b70e61a1e056b
Abdullah-Melhem <34129256+Abdullah-Melhem@users.noreply.github.com>
2024-09-13 20:19:52 -0400

    client.py

--- client.py
+++ client.py
@@ -2,14 +2,19 @@
 
 import torch
 from collections import OrderedDict
-from Abdullah.model import Net, test, train
+from Abdullah.model import Net, test, train, Net_LSTM, Net_GRU
 
 
 class Client:
-    def __init__(self, trainloder, valloader, num_class):
+    def __init__(self, clt_model, trainloder, valloader, num_class):
+        if clt_model == 'lstm':
+            self.model = Net_LSTM(num_class)
+        elif clt_model == 'gru':
+            self.model = Net_GRU(num_class)
+        else:
+            self.model = Net(num_class)
         self.trainloader = trainloder
         self.valloader = valloader
-        self.model = Net(num_class)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def set_parameters(self, parameters):
@@ -34,34 +39,25 @@ class Client:
         # copy parameters sent by the server into client's local model
         self.set_parameters(self.get_parameters())
 
-        # fetch elements in the config sent by the server. Note that having a config
-        # sent by the server each time a client needs to participate is a simple but
-        # powerful mechanism to adjust these hyperparameters during the FL process. For
-        # example, maybe you want clients to reduce their LR after a number of FL rounds.
-        # or you want clients to do more local epochs at later stages in the simulation
-        # you can control these by customising what you pass to `on_fit_config_fn` when
-        # defining your strategy.
+   
         lr = config["lr"]
         momentum = config["momentum"]
         epochs = config["local_epochs"]
 
-        # a very standard looking optimiser
-        optim = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
+       
+        optim = torch.optim.Adam(self.model.parameters(), lr=lr)
 
-        # do local training. This function is identical to what you might
-        # have used before in non-FL projects. For more advance FL implementation
-        # you might want to tweak it but overall, from a client perspective the "local
-        # training" can be seen as a form of "centralised training" given a pre-trained
-        # model (i.e. the model received from the server)
+       
         train(self.model, self.trainloader, optim, epochs, self.device)
-        # loss_val, accuracy_val = test(self.model, self.valloader, self.device)
-        # print(f" loss and accuracy for client during the validation : {loss_val, accuracy_val}")
-        # Flower clients need to return three arguments: the updated model, the number
-        # of examples in the client (although this depends a bit on your choice of aggregation
-        # strategy), and a dictionary of metrics (here you can add any additional data, but these
-        # are ideally small data structures)
-        # return self.get_parameters({}), len(self.trainloader), {}
+        
 
     def evaluate(self):
         loss, accuracy = test(self.model, self.valloader, self.device)
         return {"Loss": loss, "Accuracy": accuracy}
+
+
+def find_cluster_for_client(clusters, client):
+    for cluster, clients in clusters.items():
+        if client in clients:
+            return cluster
+    return None
